fd40e90d8bdf41c34cee5957db12ba2f5818774c
Azzurrio <just.azzurri@gmail.com>
2014-03-14 22:17:14 +0200

    Initializing the project with Home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoboStore::Application.config.secret_key_base = '50e50522b8043e5beb90d48761cda5744fbd5afa8ebbd867f218d732c19c2e1065171e4debec1782c30be7c699cd5c3ba7cb0d4da53d953251d7d026e1fdefc7'
